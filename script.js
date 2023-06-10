f1519c2a7c1114064a4a485e4b2dfec1be1abc02
KakashiHatake27 <trialacc.akshdeep@gmail.com>
2023-06-10 19:59:33 -0400

    working confetti

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-import React, {useCallback}
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+    }).then(() => jsConfetti.addConfetti())
+})
